a7e025a147e508f19a4c2c05cb74bdc69faf6084
phucdo2601 <ngocphucdo2601@gmail.com>
2023-01-10 20:22:31 +0700

    First learn basic of python b01

---
+++ app.py
@@ -0,0 +1,14 @@
+# variable in python
+# name = "Phucdn"
+# age = 23
+
+# print(name+" is a boy!")
+# print(name, " is ", age)
+# print(name+" is a from Ho Chi Minh")
+# print(name)
+
+# string in python
+name = "PHUCDN"
+print("Hi.\How are you")
+# print(name.lower().islower())
+print(len(name))
