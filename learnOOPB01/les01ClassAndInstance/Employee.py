713ea916d337af32065eac1045b060dc5634bbec
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-01-06 16:13:04 +0700

    [learn-basic-python-oop-b01] 03. Learn basic class method and statuc method in python b01

--- learnOOPB01/les01ClassAndInstance/Employee.py
+++ learnOOPB01/les01ClassAndInstance/Employee.py
@@ -18,12 +18,26 @@ class Employee:
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
+        
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+        
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, email, pay = emp_str.split('-')
+        return cls(first, last, email, pay)
+    
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        
+        return True
 
 emp_1 = Employee('Phuc', 'Do Ngoc', 'testEmailb01@gmail.com', 12313)
 emp_2 = Employee('Test-First-02', 'Test-Last-02', 'testEmailb02@gmail.com', 53534)
 
-
-print(emp_1.fullName())
 
 # print(Employee.fullName(emp_1))
 
@@ -37,12 +51,33 @@ print(emp_1.fullName())
 
 # Print instance object
 
-print(emp_1.__dict__)
-emp_1.raise_amount = 2.8
+# Employee.set_raise_amount(1.05)
 
-print(Employee.raise_amount)
+# print(emp_1.__dict__)
+# # emp_1.raise_amount = 2.8
 
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+# print(Employee.raise_amount)
 
-print("Number of employees: ",Employee.num_of_emps)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# print("Number of employees: ",Employee.num_of_emps)
+
+emp_str_1 = 'testFirstName01-testLastName01-testEmail01@gmail.com-42342'
+emp_str_2 = 'testFirstName02-testLastName02-testEmail02@gmail.com-42342'
+emp_str_3 = 'testFirstName03-testLastName03-testEmail03@gmail.com-42342'
+
+# first_name, last_name, email, pay = emp_str_1.split('-')
+
+# new_emp_1 = Employee(first_name, last_name, email, pay)
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.__dict__)
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+import datetime
+my_date = datetime.date(2023, 7, 11)
+
+print(Employee.is_workday(my_date))
